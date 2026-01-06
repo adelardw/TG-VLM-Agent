fb8f77a4a3736cdc16db2ddea20d2067067baf3e
JS <yaroslav.sergaev@gmail.com>
2026-01-06 15:15:57 +0300

    fix(agent): fix SO for notificaitions

--- src/agents/structured_outputs.py
+++ src/agents/structured_outputs.py
@@ -9,8 +9,10 @@ class CalendarStructuredOutput(BaseModel):
     description: str = Field(description="Описание события")
     location: str = Field('Moscow', description="Местоположение события, локация")
     timezone: str = Field('Europe/Moscow', description="Название временной зоны в формате IANA Time Zone Database")
-    remind_format: Literal['hours', 'days', 'weeks', 'minutes'] = Field("minutes",description="Формат повторения. Возможные значения 'hours', 'days', 'weeks', 'minutes'")
-    remind_num: int = Field(1,description="Единица времени. За сколько часов (hours), или  дней (days), или недель (weeks), или минут (minutes) нужно прислать напоминание о предстоящем событии.")
+    remind_format: Literal['hours', 'days', 'weeks', 'minutes'] = Field("minutes",description="Формат повторения. Возможные значения 'hours', 'days', 'weeks', 'minutes'." \
+                                                                        "По умолчанию повторение за минуту до события")
+    remind_num: int = Field(1,description="Единица времени. За сколько часов (hours), или  дней (days), или недель (weeks), или минут (minutes) нужно прислать напоминание о предстоящем событии."\
+                                          "По умолчанию повторение за одну минуту до события")
     remind_method: Literal['popup', 'email'] = Field('popup',description="Тип напоминания popup - увемдомление на телефон, email - на почту.")
     recurrence: list[str] = Field(description="Правила повторения события в формате iCalendar (RFC 5545)."\
                                   "Например ['RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR;COUNT=5'] (повторять 5 раз каждую неделю по понедельникам, средам и пятницам).")
